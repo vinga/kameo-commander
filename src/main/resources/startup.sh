8b06fb0ec5b715e52f178a30970562e76ee4c93d
Kamila Myczkowska <kamila.myczkowska@gmail.com>
2020-08-03 13:47:42 +0200

    changed command to run as daemon

--- src/main/resources/startup.sh
+++ src/main/resources/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pkill -f  "java -jar kameo-commander-0.0.1-SNAPSHOT.jar"
 cd /home/vinga/kameocommander
-java -jar  kameo-commander-0.0.1-SNAPSHOT.jar
+nohup java -jar  kameo-commander-0.0.1-SNAPSHOT.jar > log.log 2>&1 &
 
 # ufw allow 8111 comment 'allow kameo commander'
